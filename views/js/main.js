7d58d09615e9bbd0e21ec0a2a0ffa14f98f7d992
kodon <kathryn.odonnell@ge.com>
2015-07-19 08:17:46 -0400

    Update main.js

--- views/js/main.js
+++ views/js/main.js
@@ -505,7 +505,7 @@ function updatePositions() {
   var items = document.querySelectorAll('.mover');
   for (var i = 0; i < items.length; i++) {
     var phase = Math.sin((document.body.scrollTop / 1250) + (i % 5));
-    items[i].style.transform = 'translateX(' + items[i].basicLeft + 100 * phase + 'px' + ')'; //changed
+    items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
   }
 
   // User Timing API to the rescue again. Seriously, it's worth learning.
